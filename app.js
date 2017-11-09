96a80ceaab21536968365b43f4733d843d47f247
jogi323 <jogigj1408@gmail.com>
2017-11-10 00:57:37 +0530

    latest build

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 
@@ -54,7 +54,9 @@ app.use('/user', users);
 app.use('/availability', availabilities);
 app.use('/payments', payments);
 app.use('/offers', offers);
-
+app.get('/*',function(req,res){
+    res.sendFile(__dirname+ '/dist/index.html')
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
